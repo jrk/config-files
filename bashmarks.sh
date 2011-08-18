5e640c5119d2e48adbad6299c5cddb5279aa01e2
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-08-18 16:10:14 -0400

    Fixed completion for renamed bashmarks commands

--- bashmarks.sh
+++ bashmarks.sh
@@ -148,12 +148,12 @@ function _purge_line {
 
 # bind completion command for g,p,d to _comp
 if [ $ZSH_VERSION ]; then
-    compctl -K _compzsh g
-    compctl -K _compzsh p
-    compctl -K _compzsh d
+    compctl -K _compzsh gb
+    compctl -K _compzsh pb
+    compctl -K _compzsh db
 else
     shopt -s progcomp
-    complete -F _comp g
-    complete -F _comp p
-    complete -F _comp d
+    complete -F _comp gb
+    complete -F _comp pb
+    complete -F _comp db
 fi
